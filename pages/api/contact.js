31122067c8f4480e9838762189c4661dfb791324
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-22 18:57:39 +0100

    Validate and transport message to your email

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,14 +1,47 @@
-// pages/api/contact.js
+// Replace the code in pages/api/contact.js
 
-export default function handler(req, res) {
+import nodemailer from "nodemailer";
+
+// Replace the email and password values with a string containing your email and password
+const email = "<your-gmail-goes-here>";
+const password = "<your-gmail-password-goes-here>";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass: password,
+  },
+});
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const formValues = req.body;
+    if (
+      !formValues.username ||
+      !formValues.email ||
+      !formValues.subject ||
+      !formValues.message
+    )
+      return res
+        .status(400)
+        .json({ message: "Failed: Missing Required Values" });
     try {
-      // Function to validate email and send form values goes here
-      return res.status(200).json({ message: "Success: Message Sent" });
+      // Validate email and send form values to the validated email
+      await transporter.sendMail({
+        from: email,
+        to: email,
+        text: `
+        Name: ${formValues.username}
+        Email: ${formValues.email}
+        Message: ${formValues.message}`,
+        subject: formValues.subject,
+      });
+      return res
+        .status(200)
+        .json({ message: "Success: Message sent successfull" });
     } catch (error) {
       return res.status(400).json({
-        message: "Failed: Something went wrong. Please try again later",
+        message: `Failed: ${error.message}`,
       });
     }
   }
