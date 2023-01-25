5261eff050858c9a8aebc6026f827890a4e9a79e
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-25 11:04:02 +0000

    Fix typo

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
       });
       return res
         .status(200)
-        .json({ message: "Success: Message sent successfull" });
+        .json({ message: "Success: Message sent" });
     } catch (error) {
       return res.status(400).json({
         message: `Failed: ${error.message}`,
